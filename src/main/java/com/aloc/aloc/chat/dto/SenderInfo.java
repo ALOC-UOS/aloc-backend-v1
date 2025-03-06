7e09e6a01f72d63603ffc1e91a0275c721e91586
조수아 <suacho0724@gmail.com>
2025-03-06 10:38:43 +0900

    🔥 Remove: chat

--- src/main/java/com/aloc/aloc/chat/dto/SenderInfo.java
+++
@@ -1,3 +0,0 @@
-package com.aloc.aloc.chat.dto;
-
-public record SenderInfo(String studentId, String tier, String profileImageFileName) {}
--- src/main/java/com/aloc/aloc/chat/dto/SenderTotalInfo.java
+++
@@ -1,3 +0,0 @@
-package com.aloc.aloc.chat.dto;
-
-public record SenderTotalInfo(String studentId, SenderInfo senderInfo) {}
