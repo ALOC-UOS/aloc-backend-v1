1332bf224843c6b98428589dd1f27a869e39a585
조수아 <suacho0724@gmail.com>
2025-03-10 23:20:18 +0900

    ✏️ Fix: add dto schema info

--- src/main/java/com/aloc/aloc/course/dto/response/CourseResponseDto.java
+++ src/main/java/com/aloc/aloc/course/dto/response/CourseResponseDto.java
@@ -2,6 +2,7 @@ package com.aloc.aloc.course.dto.response;
 
 import com.aloc.aloc.course.entity.Course;
 import com.aloc.aloc.course.enums.CourseType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,16 +10,37 @@ import lombok.Getter;
 @Builder
 @Getter
 public class CourseResponseDto {
+  @Schema(description = "코스아이디", example = "1")
   private Long courseId;
+
+  @Schema(description = "코스 제목", example = "작심 삼일도 못한다고!?")
   private String title;
+
+  @Schema(description = "코스 유형", example = "DAILY")
   private CourseType courseType;
+
+  @Schema(description = "문제 수", example = "3")
   private int problemCnt;
+
+  @Schema(description = "최소 랭크", example = "1")
   private int minRank;
+
+  @Schema(description = "최대 랭크", example = "5")
   private int maxRank;
+
+  @Schema(description = "평균 난이도", example = "3")
   private int averageRank;
+
+  @Schema(description = "코스 생성수(인기순)", example = "10")
   private Long generateCnt;
+
+  @Schema(description = "마감기한", example = "3")
   private int duration;
+
+  @Schema(description = "생성일", example = "2024-03-04T19:37:55")
   private LocalDateTime createdAt;
+
+  @Schema(description = "유저의 코스 성공 여부", example = "false")
   private boolean isSuccess;
 
   public static CourseResponseDto of(Course course, boolean isSuccess) {
