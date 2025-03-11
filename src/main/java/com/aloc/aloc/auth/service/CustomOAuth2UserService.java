173ce980ea406a707de43ac16f5bbebb45a37b23
조수아 <suacho0724@gmail.com>
2025-03-11 23:01:32 +0900

    ✏️ Fix: add log

--- src/main/java/com/aloc/aloc/auth/service/CustomOAuth2UserService.java
+++ src/main/java/com/aloc/aloc/auth/service/CustomOAuth2UserService.java
@@ -7,6 +7,7 @@ import com.aloc.aloc.user.repository.UserRepository;
 import java.util.Collections;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
   private final UserRepository userRepository;
@@ -25,6 +27,14 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
     OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+
+	// ✅ user-info-uri 값 확인
+    String userInfoEndpointUri = userRequest.getClientRegistration()
+                                           .getProviderDetails()
+                                           .getUserInfoEndpoint()
+                                           .getUri();
+
+    log.info("🔍 UserInfo 요청 URI: {}", userInfoEndpointUri); // ✅ 올바른지 로그 확인
     OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
     // OAuth 서비스 이름(ex. google, naver, kakao)
