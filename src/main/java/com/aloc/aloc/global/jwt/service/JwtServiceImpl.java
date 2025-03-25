8ae8edb35ce76f686dbe87fc7c1906654089375c
조수아 <suacho0724@gmail.com>
2025-03-25 10:34:08 +0900

    ✏️ Fix: add log

--- src/main/java/com/aloc/aloc/global/jwt/service/JwtServiceImpl.java
+++ src/main/java/com/aloc/aloc/global/jwt/service/JwtServiceImpl.java
@@ -69,8 +69,11 @@ public class JwtServiceImpl implements JwtService {
             users -> {
               users.updateRefreshToken(refreshToken);
               userRepository.save(users);
+              log.info("✅ [updateRefreshToken] refreshToken 저장 성공 - user: {}", users.getOauthId());
             },
-            () -> new Exception("회원 조회 실패"));
+            () -> {
+              throw new RuntimeException("❌ 회원 조회 실패: oauthId = " + oauthId);
+            });
   }
 
   @Override
