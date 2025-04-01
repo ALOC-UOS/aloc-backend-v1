4c562a30198e0f1516299e4d5ad9b1514affaab6
조수아 <suacho0724@gmail.com>
2025-04-01 19:51:18 +0900

    ✏️ Fix: redirect

--- src/main/java/com/aloc/aloc/auth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/aloc/aloc/auth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -37,8 +37,16 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
     jwtService.updateRefreshToken(oauthId, refreshToken);
     jwtService.setRefreshTokenCookie(response, refreshToken);
 
-    //    String redirectUri = "https://openaloc.store/finish-google-sso";
-    String redirectUri = "https://localhost:3000/finish-google-sso";
+    // 요청 도메인 체크
+    String host = request.getHeader("Host"); // 예: openaloc.store 또는 localhost:8080
+    log.info("요청 Host: {}", host);
+
+    String redirectUri;
+    if (host != null && host.contains("openaloc.store")) {
+      redirectUri = "https://openaloc.store/finish-google-sso";
+    } else {
+      redirectUri = "http://localhost:3000/finish-google-sso";
+    }
 
     // ✅ 쿼리파라미터로 토큰 전달 (주의: refreshToken은 보안상 권장 안됨!)
     String redirectWithToken = String.format("%s?accessToken=%s", redirectUri, accessToken);
--- src/main/java/com/aloc/aloc/global/config/SecurityConfig.java
+++ src/main/java/com/aloc/aloc/global/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -43,6 +44,8 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
         .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(
             jwtAuthenticationProcessingFilter(), LogoutFilter.class) // ✅ UsernamePassword 관련 필터 제거
         .authorizeHttpRequests(
